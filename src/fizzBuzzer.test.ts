b9dabf409b3f3c391e2e4de36904a8bb85c6039b
cynthiacodes <cjnkstudy01@hotmail.com>
2023-09-13 10:15:18 +0100

    added test for reset method

--- src/fizzBuzzer.test.ts
+++ src/fizzBuzzer.test.ts
@@ -7,3 +7,13 @@ test(".next() method in FizzBuzzer returns Fizz for numbers divisible by 3", () 
     expect(myFizzBuzzer.next()).toBe(2);
     expect(myFizzBuzzer.next()).toBe("Fizz");
 });
+
+test(".reset() method in FizzBuzzer resets this.value back to zero so the next() method returns 1", () => {
+    const myFizzBuzzer = new FizzBuzzer();
+
+    expect(myFizzBuzzer.next()).toBe(1);
+    expect(myFizzBuzzer.next()).toBe(2);
+    expect(myFizzBuzzer.next()).toBe("Fizz");
+    myFizzBuzzer.reset();
+    expect(myFizzBuzzer.next()).toBe(1);
+});
