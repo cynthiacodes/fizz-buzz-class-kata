f12f8940c058ee8edd07d0e16bddd1bdf35d70b5
cynthiacodes <cjnkstudy01@hotmail.com>
2023-09-13 10:38:00 +0100

    added test for myBizzFuzzer

--- src/fizzBuzzer.test.ts
+++ src/fizzBuzzer.test.ts
@@ -1,4 +1,4 @@
-import { FizzBuzzer } from "./fizzBuzzer";
+import { BizzFuzzer, FizzBuzzer } from "./fizzBuzzer";
 
 test(".next() method in FizzBuzzer returns Fizz for numbers divisible by 3", () => {
     const myFizzBuzzer = new FizzBuzzer();
@@ -16,4 +16,18 @@ test(".reset() method in FizzBuzzer resets this.value back to zero so the next()
     expect(myFizzBuzzer.next()).toBe("Fizz");
     myFizzBuzzer.reset();
     expect(myFizzBuzzer.next()).toBe(1);
+});
+
+test("BizzFuzzer, which takes two words to be used in place of “fizz” and “buzz”, and two numbers to be used in place of 3 and 5", () => {
+    const myBizzFuzzer = new BizzFuzzer("Boop", "Schnoop", 2, 3);
+
+    expect(myBizzFuzzer.next()).toBe(1);
+    expect(myBizzFuzzer.next()).toBe("Boop");
+    expect(myBizzFuzzer.next()).toBe("Schnoop");
+    expect(myBizzFuzzer.next()).toBe("Boop");
+    expect(myBizzFuzzer.next()).toBe(5);
+    expect(myBizzFuzzer.next()).toBe("BoopSchnoop");
+
+    myBizzFuzzer.reset();
+    expect(myBizzFuzzer.next()).toBe(1);
 });
